ffbb35432b09d85e8cb7f0ce04fdd81946881e32
wilmer <wilmeryes96@yahoo.es>
2023-05-26 02:37:45 +0300

    script to remove dependencies

---
+++ delete.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Global variables
+MYSQL_IMAGE_NAME="mysql-db-mt-image"
+JAVA_IMAGE_NAME="java17-api-mt-image"
+
+NETWORK_NAME="money-transfer-docker-network"
+
+VOLUME_NAME="mysql-mt-volume"
+VOLUME_PATH=$VOLUME_NAME":/var/lib/mysql"
+MYSQL_CONTAINER_NAME="mysql-db-mt-container"
+JAVA_API_CONTAINER_NAME="java17-api-mt-container"
+
+MYSQL_DATABASE_NAME="money_transfer_db"
+
+docker stop $MYSQL_CONTAINER_NAME
+docker rm $MYSQL_CONTAINER_NAME
+
+docker stop $JAVA_API_CONTAINER_NAME
+docker rm $JAVA_API_CONTAINER_NAME
+
+docker volume prune
+
+docker network rm $NETWORK_NAME
+
+docker rmi $MYSQL_IMAGE_NAME
+docker rmi $JAVA_IMAGE_NAME
